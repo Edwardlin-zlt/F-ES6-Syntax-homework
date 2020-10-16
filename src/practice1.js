52bb0725e42324104aadedd7b1a0e1a7e23ef68c
lintao <lintao.zhang@thoughtworks.com>
2020-10-16 16:25:25 +0800

    [Lintao] feat: homework finish

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
-export { parseData };
+const parseData = (input) => {
+  const {data, column} = input;
+  const fields = getFields(column);
+  return data.map(rowData => rowDataToObject(rowData, fields))
+}
+
+const getFields = (structure) => structure.map(obj => obj.name);
+
+const rowDataToObject = (rowData, fields) => {
+  return rowData.reduce((pre, cur, index) => {
+    pre[fields[index]] = cur;
+    return pre;
+  }, {})
+}
+
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,11 @@
-const inject
+const inject = (items, sections) => {
+  return items.reduce((pre, cur, index) => {
+    let matchedObjList = sections.filter(obj => obj.index === index)
+    if (matchedObjList[0]) {
+      pre.push(matchedObjList[0].content);
+    }
+    pre.push(cur)
+    return pre;
+  }, [])
+}
 export { inject };
